d4af27b42a627f6ef057c4697b3dfbee4ee8fb93
sharath9491 <124574754+sharath9491@users.noreply.github.com>
2023-06-24 23:27:40 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch31 team with joy datta belli ananda , rushwanth  and team");
+     System.out.println("Hello batch31 team with bangara chinna ranna  , rushwanth  and team");
       System.out.println("Hello Batch batch30 WITH FOR STUDENTS RUNNING in front of lion and Anita and suchandra sindhu in making CI automated vikas teach devops CI proecss  ");
     }  
 } 
